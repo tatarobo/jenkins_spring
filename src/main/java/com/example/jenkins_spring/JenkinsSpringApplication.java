d6f833730005626f9b25facc3174d8115462cefd
Jihyun.jeong <jihyun.jeong@nhn.com>
2022-06-24 18:17:25 +0900

    default

---
+++ src/main/java/com/example/jenkins_spring/JenkinsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkins_spring/JenkinsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
